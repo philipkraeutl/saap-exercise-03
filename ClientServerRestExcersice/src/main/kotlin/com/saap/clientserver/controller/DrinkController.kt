cb920833abddf3191c76ab2f1837c6063d16a097
Kräutl Philip <philip.kraeutl@gmail.com>
2022-11-03 14:18:28 +0100

    Update DrinkController.kt

--- ClientServerRestExcersice/src/main/kotlin/com/saap/clientserver/controller/DrinkController.kt
+++ ClientServerRestExcersice/src/main/kotlin/com/saap/clientserver/controller/DrinkController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,6 +19,18 @@ class DrinkController(private val calculatorService: CalculatorService) {
     fun insertDrink(@RequestBody drink: Drink): ResponseEntity<List<Drink>> {
         CalculatorService.drinks.add(drink)
         return ResponseEntity(CalculatorService.drinks, HttpStatus.OK)
+    }
+
+    @PutMapping("/drink/byName")
+    fun updateDrinkName(oldName: String, newName: String): ResponseEntity<Drink> {
+        CalculatorService.drinks.let { drinks ->
+            val drink = drinks.find { it.name.equals(oldName, true) } ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+            val index = drinks.indexOf(drink)
+            drinks.remove(drink)
+            val newDrink = drink.copy(name = newName)
+            drinks.add(index,newDrink)
+            return ResponseEntity(newDrink, HttpStatus.OK)
+        }
     }
 
     @DeleteMapping("/drink/deleteLast")
